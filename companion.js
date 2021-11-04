0769a5ba74946e0549acae01fef9eb827383f2fc
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2021-11-04 05:35:19 -0500

    Refactor index & companion

--- companion.js
+++ companion.js
@@ -1,19 +1,29 @@
+let companionImage = document.getElementById("companion");
+let goalButton = document.getElementById("goalButton");
+let taskButton = document.getElementById("taskButton");
+let breakButton = document.getElementById("breakButton");
+
 //Start waving animation after page loads 
-window.onload = waving(); 
+window.addEventListener("load", waving);
+
+//Change animation when buttons are clicked
+goalButton.addEventListener("click", talking); 
+taskButton.addEventListener("click", talking); 
+breakButton.addEventListener("click", talking); 
 
 //Waving animation 
 function waving(){
-    document.getElementById("companion").src = "images/waving-fullsize.gif";
+    companionImage.src = "images/waving-fullsize.gif";
     setTimeout(idle, 4000);
 }
 
 //Talking animation
 function talking(){
-    document.getElementById("companion").src = "images/talking-fullsize.gif";
+    companionImage.src = "images/talking-fullsize.gif";
     setTimeout(idle, 9500);
 }
 
 //Idle animation
 function idle(){
-    document.getElementById("companion").src = "images/blinking-fullsize.gif";
+    companionImage.src = "images/blinking-fullsize.gif";
 }
