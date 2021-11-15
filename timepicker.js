7b6364265799e6163645b2f52f60cfd951d0136b
armijosj <juannarmijos@gmail.com>
2021-11-14 21:14:25 -0500

    Task name updated
    Task name updated

--- timepicker.js
+++ timepicker.js
@@ -24,16 +24,3 @@ var tpSpinboxBreak = new tui.TimePicker('#timepicker-selectbox-task', {
     inputType: 'selectbox',
     showMeridiem: false
 });
-
-//Add goal name to left box.
-function updateGoal(){
-    let gName = document.getElementById("gname").value;
-    document.getElementById("currGoal").innerHTML = "Current Goal: " + gName;
-}
-
-//Add task name to left box.
-function updateTask(){
-    //let tName = document.getElementById("tname").value;
-    //document.getElementById("currTask").innerHTML = "Current Task: " + tName;
-    //uncomment when input for task is done.
-}
