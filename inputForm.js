a18ed80070935faa399da2018d0684c6480bcba1
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2021-12-20 04:58:46 -0600

    make datePicker functional

--- inputForm.js
+++ inputForm.js
@@ -692,47 +692,46 @@ let monthlyChartData = [];
 
 let datePicker = document.querySelector("#trends--datePicker");
 
-
 $("#datepicker").datepicker({ 
-      onSelect: function(value, date) { 
-         alert('The chosen date is ' + value); 
+    maxDate: 0, 
+    changeMonth: true,
+    changeYear: true,
+    showButtonPanel: true,
+    
+    onSelect: function(value, date) { 
+        var month = value.toString().substr(0, 2) - 1; 
+        var date = value.toString().substr(3, 2); 
+        var year = value.toString().substr(6, 4); 
 
-         // Hide the datepicker div when something is selected
-
-         //$("#datepicker").hide(); 
-         //modal.style.display = "none";
-      } 
+        if(currChart.id == "weeklyChart") {
+            currWeek = new Date(year,month,date);
+            datePicker_Weekly();
+        }
+        else if(currChart.id == "dailyChart") {
+            currDate = new Date(year,month,date);
+            datePicker_Daily();
+        }
+        else if(currChart.id == "monthlyChart") {
+            currMonth = new Date(year,month,date);
+            datePicker_Monthly();
+        }
+    } 
 });
 
-
-// Get the modal
-//var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("datePickerButton");
+var span = document.getElementsByClassName("close")[0];
+var popup = document.getElementById("myPopup");
 
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  //modal.style.display = "block";
-
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+function toggleCalendar() {
+    if(popup.style.visibility == "hidden") {
+        popup.style.visibility = "visible";
+    } else
+        popup.style.visibility = "hidden";
 }
 
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+function hideCalendar() {
+    popup.style.visibility = "hidden";
+}
 
 function updateDailyChart() {
 
