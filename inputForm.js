cbd68d192632ce23fe216534f614f8a736edb41d
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2021-12-20 02:12:28 -0600

    add calendar picker

--- inputForm.js
+++ inputForm.js
@@ -689,7 +689,51 @@ function convertSecToFormat(timeInSecs) {
 let dailyChartData = []; 
 let weeklyChartData = []; 
 let monthlyChartData = []; 
-  
+
+let datePicker = document.querySelector("#trends--datePicker");
+
+
+$("#datepicker").datepicker({ 
+      onSelect: function(value, date) { 
+         alert('The chosen date is ' + value); 
+
+         // Hide the datepicker div when something is selected
+
+         //$("#datepicker").hide(); 
+         //modal.style.display = "none";
+      } 
+});
+
+
+// Get the modal
+//var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("datePickerButton");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  //modal.style.display = "block";
+
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
 function updateDailyChart() {
 
     let goals = cb.goalsList;
